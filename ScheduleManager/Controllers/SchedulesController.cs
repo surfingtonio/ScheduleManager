4646e9364f0d37563a95dcc17b02daa712428171
Marc Danting <marc@danting.ca>
2019-05-09 14:18:49 -0700

    Generated Schedules controller and views.

---
+++ ScheduleManager/Controllers/SchedulesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ScheduleManager.Data;
+using ScheduleManager.Data.Entities;
+
+namespace ScheduleManager.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private readonly ScheduleManagerDbContext _context;
+
+        public SchedulesController(ScheduleManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Schedules
+        public async Task<IActionResult> Index()
+        {
+            var scheduleManagerDbContext = _context.Schedules.Include(s => s.Employee);
+            return View(await scheduleManagerDbContext.ToListAsync());
+        }
+
+        // GET: Schedules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Employee)
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // GET: Schedules/Create
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId");
+            return View();
+        }
+
+        // POST: Schedules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ScheduleId,StartDate,EndDate,RepeatAfterDays,EmployeeId")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(schedule);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", schedule.EmployeeId);
+            return View(schedule);
+        }
+
+        // GET: Schedules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", schedule.EmployeeId);
+            return View(schedule);
+        }
+
+        // POST: Schedules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ScheduleId,StartDate,EndDate,RepeatAfterDays,EmployeeId")] Schedule schedule)
+        {
+            if (id != schedule.ScheduleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(schedule);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ScheduleExists(schedule.ScheduleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", schedule.EmployeeId);
+            return View(schedule);
+        }
+
+        // GET: Schedules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Employee)
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // POST: Schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var schedule = await _context.Schedules.FindAsync(id);
+            _context.Schedules.Remove(schedule);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ScheduleExists(int id)
+        {
+            return _context.Schedules.Any(e => e.ScheduleId == id);
+        }
+    }
+}
