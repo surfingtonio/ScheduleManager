77ce2238d95878cee21e48422241f8d15b26d7de
Marc Danting <marc@danting.ca>
2019-05-09 12:11:42 -0700

    Add project files.

---
+++ ScheduleManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ScheduleManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
