f4324274439e07478fdfd6d19fd99ab40b1bff77
Marc Danting <marc@danting.ca>
2019-05-10 10:59:40 -0700

    Added Title column to Schedules table.

---
+++ ScheduleManager/Migrations/20190510175610_AddedTitleColumnToSchedulesTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScheduleManager.Migrations
+{
+    public partial class AddedTitleColumnToSchedulesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Schedules",
+                nullable: false,
+                defaultValue: "Schedule Title");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Schedules");
+        }
+    }
+}
