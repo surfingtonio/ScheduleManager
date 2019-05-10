c528ddc43154e74043a4414da8e56b6bbcd3ebd4
Marc Danting <marc@danting.ca>
2019-05-10 14:01:11 -0700

    Added CalendarController and index view.

---
+++ ScheduleManager/Controllers/CalendarController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ScheduleManager.Data;
+using ScheduleManager.Data.Entities;
+using ScheduleManager.Models;
+
+namespace ScheduleManager.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private readonly ScheduleManagerDbContext _context;
+
+        public CalendarController(ScheduleManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = new CalendarViewModel
+            {
+                Employees = _context.Employees.ToList(),
+                SelectedEmployee = _context.Employees.FirstOrDefault()
+            };
+
+            return View(data);
+        }
+    }
+}
---
+++ ScheduleManager/Models/CalendarViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ScheduleManager.Data.Entities;
+using System.Collections.Generic;
+
+namespace ScheduleManager.Models
+{
+    public class CalendarViewModel
+    {
+        public CalendarViewModel()
+        {
+            Employees = new List<Employee>();
+        }
+
+        public Employee SelectedEmployee { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
