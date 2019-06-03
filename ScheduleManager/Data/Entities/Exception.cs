a7510aece93323ff337a219f87cc4161d0f2bbff
Marc Danting <marc@danting.ca>
2019-06-03 16:10:23 -0700

    Created Exceptions table.

---
+++ ScheduleManager/Data/Entities/Exception.cs
@@ -0,0 +1,11 @@
+﻿namespace ScheduleManager.Data.Entities
+{
+    public class Exception : Event
+    {
+        public int ExceptionId { get; set; }
+
+        public int ScheduleId { get; set; }
+
+        public virtual Schedule Schedule { get; set; }
+    }
+}
---
+++ ScheduleManager/Migrations/20190603230905_CreatedExceptionsTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScheduleManager.Migrations
+{
+    public partial class CreatedExceptionsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exceptions",
+                columns: table => new
+                {
+                    ExceptionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    IsAllDay = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Frequency = table.Column<int>(nullable: false),
+                    Interval = table.Column<int>(nullable: false),
+                    IsOnWeekdays = table.Column<bool>(nullable: false),
+                    RecurUntil = table.Column<DateTime>(nullable: true),
+                    RepeatsIndefinitely = table.Column<bool>(nullable: false),
+                    CanOccurOnHolidays = table.Column<bool>(nullable: false),
+                    ScheduleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exceptions", x => x.ExceptionId);
+                    table.ForeignKey(
+                        name: "FK_Exceptions_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "ScheduleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exceptions_ScheduleId",
+                table: "Exceptions",
+                column: "ScheduleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exceptions");
+        }
+    }
+}
