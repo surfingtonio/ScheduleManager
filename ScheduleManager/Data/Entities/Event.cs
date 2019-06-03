cc86c8536c70ac11fb0a6e09fe2933d6d78740e9
Marc Danting <marc@danting.ca>
2019-06-03 16:06:39 -0700

    Added Event class.

---
+++ ScheduleManager/Data/Entities/Event.cs
@@ -0,0 +1,72 @@
+﻿using EWSoftware.PDI;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ScheduleManager.Data.Entities
+{
+    public class Event
+    {
+        public Event()
+        {
+            Frequency = RecurFrequency.Weekly;
+            Interval = 1;
+            RepeatsIndefinitely = true;
+        }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public bool IsAllDay { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        // Recurrence properties
+
+        public RecurFrequency Frequency { get; set; }
+
+        public int Interval { get; set; }
+
+        public bool IsOnWeekdays { get; set; }
+
+        public DateTime? RecurUntil { get; set; }
+
+        public bool RepeatsIndefinitely { get; set; }
+
+        public bool CanOccurOnHolidays { get; set; }
+
+        public virtual DateTimeCollection GetInstancesBetween(DateTime startDate, DateTime endDate)
+        {
+            var r = new Recurrence
+            {
+                StartDateTime = StartDate,
+                Frequency = Frequency,
+                CanOccurOnHoliday = CanOccurOnHolidays
+            };
+
+            if (!RepeatsIndefinitely)
+            {
+                if (RecurUntil == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                r.RecurUntil = (DateTime)RecurUntil;
+            }
+
+            if (IsOnWeekdays)
+            {
+                r.RecurEveryWeekday();
+            }
+            else
+            {
+                r.Interval = Interval;
+            }
+
+            return r.InstancesBetween(startDate, endDate);
+        }
+    }
+}
